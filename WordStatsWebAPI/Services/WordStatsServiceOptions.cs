7b095d0a3921a4c3c357539dd3a48aa84d0a76e0
Heshan <heshan.chiu@gmail.com>
2023-08-06 23:18:15 +1200

    Daily commit

---
+++ WordStatsWebAPI/Services/WordStatsServiceOptions.cs
@@ -0,0 +1,8 @@
+namespace WordStats.Services
+{
+    public class WordStatsServiceOptions
+    {
+        public int ReadDelay { get; set; } = 1000;
+        public int WriteDelay { get; set; } = 1000;
+    }
+}
