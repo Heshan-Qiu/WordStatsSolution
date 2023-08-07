80b1e9b8803925a2156c96bafb18bc56ddde86fa
Heshan <heshan.chiu@gmail.com>
2023-08-07 14:33:08 +1200

    Daily commit.

--- WordStatsWebAPI/Program.cs
+++ WordStatsWebAPI/Program.cs
@@ -1,43 +1,46 @@
+using System.Text;
+
+using RandomWords;
+using WordStats.Interfaces;
+using WordStats.Implements;
+using WordStats.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new() { Title = "Word Stats Backend API", Description = "Word Stats Backend API", Version = "v1" });
+});
+
+builder.Services.AddSingleton<Stream, RandomWordStream>();
+builder.Services.AddSingleton<Encoding, UTF8Encoding>();
+builder.Services.AddSingleton<IWordStats, WordStatsDictionaryImplement>();
+builder.Services.AddSingleton<IWordStatsWriter, WordStatsWriterConsoleImplement>();
+builder.Services.Configure<WordStatsServiceOptions>(builder.Configuration.GetSection("WordStatsService"));
+builder.Services.AddHostedService<WordStatsService>();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", builder =>
+    {
+        builder.AllowAnyOrigin();
+        builder.AllowAnyMethod();
+        builder.AllowAnyHeader();
+    });
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    app.UseDeveloperExceptionPage();
 
-app.UseHttpsRedirection();
+app.UseSwagger();
+app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Word Stats Backend API v1"));
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+app.UseCors("AllowAll");
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateTime.Now.AddDays(index),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+app.MapGet("/", () => "Hello WordStats App!");
+app.MapGet("/stats", () => app.Services.GetService<IWordStats>()?.ToJsonString());
 
 app.Run();
-
-record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
